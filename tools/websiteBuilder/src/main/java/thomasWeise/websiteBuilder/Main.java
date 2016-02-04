84a00fcf7da4d892ae8de6a92363f5c409a2e4ed
Thomas Weise <tweise@ustc.edu.cn>
2016-02-04 10:39:02 +0800

    First version of tool set: does nothing yet, just project structure

---
+++ tools/websiteBuilder/src/main/java/thomasWeise/websiteBuilder/Main.java
@@ -0,0 +1,78 @@
+package thomasWeise.websiteBuilder;
+
+import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.optimizationBenchmarking.utils.config.Configuration;
+
+/** The main class. */
+public class Main {
+
+  /** the source argument */
+  public static final String PARAM_SOURCE = "source";//$NON-NLS-1$
+  /** the destination argument */
+  public static final String PARAM_DEST = "dest";//$NON-NLS-1$
+
+  /**
+   * The main entry point of the website builder
+   * 
+   * @param args
+   *          the command line arguments: 1) input folder, 2) output folder
+   */
+  public static void main(String[] args) {
+    final Logger logger;
+    final Configuration config;
+    final Path source, dest;
+
+    Configuration.setup(args);
+    config = Configuration.getRoot();
+    logger = Configuration.getGlobalLogger();
+
+    if ((logger != null) && (logger.isLoggable(Level.INFO))) {
+      logger.info("Welcome to the website builder."); //$NON-NLS-1$
+    }
+
+    source = config.getPath(PARAM_SOURCE, null);
+    if (source == null) {
+      if (logger != null) {
+        logger.severe(
+            "Must specify source folder with argument 'source=...'."); //$NON-NLS-1$
+      }
+      return;
+    }
+
+    dest = config.getPath(PARAM_DEST, null);
+    if (dest == null) {
+      if (logger != null) {
+        logger.severe(
+            "Must specify destination folder with argument 'dest=...'."); //$NON-NLS-1$
+      }
+      return;
+    }
+
+    build(source, dest, logger);
+  }
+
+  /**
+   * Build the website
+   * 
+   * @param source
+   *          the source folder
+   * @param dest
+   *          the destination folder
+   * @param logger
+   *          the logger
+   */
+  public static final void build(final Path source, final Path dest,
+      final Logger logger) {
+    if ((logger != null) && (logger.isLoggable(Level.INFO))) {
+      logger.info("Building website from source '" + source + //$NON-NLS-1$
+          "' to dest '" + dest + '\'' + '.');//$NON-NLS-1$
+    }
+
+    if ((logger != null) && (logger.isLoggable(Level.INFO))) {
+      logger.info("Finished building website.");//$NON-NLS-1$
+    }
+  }
+}
